66bf255dd76976c9be021ff853f3a40d51cac1cd
wagnerdantas <wagner.trc@gmail.com>
2023-03-29 02:53:02 -0300

    1

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -3,6 +3,6 @@
 describe('Central de Atendimento ao Cliente TAT', function () {
   it('verifica o título da aplicação', function () {
     cy.visit('./src/index.html')
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT1')
   })
 })
